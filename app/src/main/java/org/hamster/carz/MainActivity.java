3583a48aa2f528423be87289d04b513866eac92b
Hamster Tian <haotia@gmail.com>
2015-12-21 12:43:35 +0800

    BUG FIX: Controller fragment is not fullscreen
    Do not reserve space for SystemUI bars since they are gone.
    (CoordinatorLayout draws bars itself (or with other widgets) so we need to tell it not to reserve this space)

--- app/src/main/java/org/hamster/carz/MainActivity.java
+++ app/src/main/java/org/hamster/carz/MainActivity.java
@@ -315,6 +315,7 @@ public class MainActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        mRootView.setFitsSystemWindows(false);
         Animation animSlideUp = AnimationUtils.loadAnimation(this, R.anim.slide_up);
         mToolbar.startAnimation(animSlideUp);
         getSupportActionBar().hide();
@@ -322,6 +323,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void showBars() {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        mRootView.setFitsSystemWindows(true);
         Animation animSlideDown = AnimationUtils.loadAnimation(this, R.anim.slide_down);
         mToolbar.startAnimation(animSlideDown);
         getSupportActionBar().show();
